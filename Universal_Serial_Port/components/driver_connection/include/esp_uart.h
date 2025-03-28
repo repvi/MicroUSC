81b1a03d17fa741c8c589ed7b8848e01f5e3a810
Alejandro Ramirez <139709985+ASM-GP@users.noreply.github.com>
2025-03-28 18:31:08 -0500

    Main Program code
    USC (Universal Serial Connecttion/Control)

---
+++ Universal_Serial_Port/components/driver_connection/include/esp_uart.h
@@ -0,0 +1,40 @@
+#ifndef __ESP_UART_SETTING_H
+#define __ESP_UART_SETTING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "driver/gpio.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "esp_log.h"
+#include "esp_err.h"
+
+/**
+ * @struct uart_port_config_t
+ * @brief Configuration structure for UART ports.
+ */
+typedef struct {
+    gpio_num_t tx;  ///< GPIO pin for UART transmit.
+    gpio_num_t rx;  ///< GPIO pin for UART receive.
+    uart_port_t port; ///< UART port identifier.
+} uart_port_config_t;
+
+typedef char* serial_data_ptr_t;
+
+typedef unsigned int serial_size_t;
+
+typedef int baud_rate_t;
+
+esp_err_t uart_init(uart_port_config_t port_config, uart_config_t uart_config);
+
+serial_data_ptr_t uart_read(uart_port_t *uart, size_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
