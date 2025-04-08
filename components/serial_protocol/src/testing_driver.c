d8fb78875b4ba439d0f0ec71d2a2e6a6f395e686
ABushman88 <bush5418@gmail.com>
2025-04-08 16:59:19 -0500

    Tetsting commit

--- components/serial_protocol/src/testing_driver.c
+++ components/serial_protocol/src/testing_driver.c
@@ -3,6 +3,19 @@
 #include "stddef.h"
 #include "stdint.h"
 
+/* n = 0 -> default value 
+   n = 1 -> turn off esp32
+   n = 2 -> sleep mode
+   n = 3 -> pause port data
+   n = 4 -> connect to wifi
+   n = 5 -> connect to bluetooth
+   n = 6 -> turn on built-in LED on esp32
+   n = 7 -> memory usage 
+   n = 8 -> system specs
+   n = 9 -> driver status
+   n = 10 -> overdriver status
+*/
+
 uint32_t getData(void) {
     return 1;
 }
@@ -12,7 +25,30 @@ void function(void *p) {
 
     while (1) {
         data = getData();
-        if ()
+        
+        switch(data) {
+            case 1: 
+                
+                break;
+            case 2:
+                break;
+            case 3: 
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+        }
     }
 }
 
